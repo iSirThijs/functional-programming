b4015d0a3074be09ac953f99e68339b92d9e8239
Thijs Spijker <thijs.spijker@hva.nl>
2019-11-08 09:44:34 +0100

    add boilerplate

---
+++ main.mjs
@@ -0,0 +1,10 @@
+import { queryStringUtilities } from './git_modules/nmvw-sparql-module/nmvw-sparql-module.mjs';
+
+let prefixes = {
+	dc: '<http://purl.org/dc/elements/1.1/>',
+	skos: '<http://www.w3.org/2004/02/skos/core#>',
+	dct: '<http://purl.org/dc/terms/>',
+	edm: '<http://www.europeana.eu/schemas/edm/>',
+	rdf: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#>',
+	rdfs: '<http://www.w3.org/2000/01/rdf-schema#>',
+}
